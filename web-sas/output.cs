164d41352155af888aedc1fea7db8fd6b0d4b8cf
Arsyad Basofil Amri <arsyadbasofilamrii@gmail.com>
2023-04-04 11:00:45 +0700

    update sass

---
+++ web-sas/output.cs
@@ -0,0 +1,75 @@
+body {
+  font: 100, "helvetica", "san-serif";
+  margin: 0;
+}
+
+.nav {
+  background-color: #ffffff;
+  color: #707070;
+  height: 50px;
+}
+.nav a {
+  float: left;
+  display: block;
+  text-align: center;
+  padding: 10px 20px;
+  text-decoration: none;
+}
+.nav a img {
+  height: 25px;
+  width: 25px;
+}
+.nav a:hover {
+  background-color: #ff0000;
+  color: #5300b3;
+  padding: 10px 20px;
+}
+.nav .right {
+  display: block;
+  float: right;
+  text-align: center;
+  text-decoration: none;
+}
+
+.head {
+  padding: 20px;
+  background-image: url(/web-sas/kitten.jpg);
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+  border-top: 1px solid #5300b3;
+  border-bottom: 1px solid #ff0000;
+  color: #ffffff;
+  height: 400px;
+}
+
+.header-text h1 {
+  text-align: center;
+}
+
+.about {
+  background-color: #707070;
+  color: #5300b3;
+  height: 50px;
+}
+
+.product {
+  background-color: #ff0000;
+  color: #ffffff;
+  height: 50px;
+}
+
+.contact {
+  background-color: #ffffff;
+  color: #707070;
+  height: 50px;
+}
+
+.footer {
+  background-color: #5300b3;
+  color: #ffffff;
+  height: 50px;
+}
+
+/*# sourceMappingURL=output.cs.map */
